64147eb2d7e071be3207d19823a73dfdaa4120be
KarolinaFilipiuk <karolina.filipiuk@interia.com>
2022-07-23 17:47:24 +0200

    [GREEN] Added popover

--- sundaes-on-demand-front/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand-front/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
@@ -34,7 +38,7 @@ test('Checkbox enables button on first click and disables on second click', () =
   expect(confirmButton).toBeDisabled();
 });
 
-test('popover responds to hover', () => {
+test('popover responds to hover', async () => {
   render(<SummaryForm />);
 
   // popover starts out hidden
@@ -51,8 +55,7 @@ test('popover responds to hover', () => {
 
   // popover disappears when we mouse out
   userEvent.unhover(termsAndConditions);
-  const nullPopoverAgain = screen.queryByText(
-    /no ice cream will actually be delivered/i
+  await waitForElementToBeRemoved(
+    screen.queryByText(/no ice cream will actually be delivered/i)
   );
-  expect(nullPopoverAgain).not.toBeInTheDocument();
 });
